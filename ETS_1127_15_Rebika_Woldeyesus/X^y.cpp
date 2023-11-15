9f554161544d37e15c09da1f342951cb39846a45
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-11-15 11:32:41 +0300

    Update X^y.cpp

--- ETS_1127_15_Rebika_Woldeyesus/X^y.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/X^y.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//this is a code to calculate x^(y);
+int main() {
+    double x, y, result;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+    result = pow(x, y);
+    cout << "The result of " << x << " raised to the power of " << y << " is: " << result << endl;
 
+    return 0;
+}
