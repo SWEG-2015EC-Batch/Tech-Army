b71b64039ab0ecdc964863b488b18a203817d159
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2023-11-15 11:12:19 +0300

    Create Leter.cpp

---
+++ ETS1137_15_Rediet_Geremew/Leter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+int main() {
+    char inputLetter;
+
+/*this is a code that changes an e tered letter
+to its uppercase or lowercase;*/
+    cout << "Enter a letter: ";
+    cin >> inputLetter;
+    if (isalpha(inputLetter)) {
+    if (islower(inputLetter)) {
+    cout << "Uppercase equivalent: " << static_cast<char>(toupper(inputLetter)) << endl;
+        }
+   
+     else if (isupper(inputLetter)) {
+      cout << "Lowercase equivalent: " << static_cast<char>(tolower(inputLetter)) << endl;
+        }
+    }
+   
+    else {
+        cout << "Please enter a valid letter." << endl;
+    }
+
+    return 0;
+}
