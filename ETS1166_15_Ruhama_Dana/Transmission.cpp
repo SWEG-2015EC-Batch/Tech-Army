87be3b570a922789f97f4d28a6e542821a41e802
Ruhamafikre985 <149158207+Ruhamafikre985@users.noreply.github.com>
2023-11-15 10:42:05 +0300

    Create Transmission.cpp

---
+++ ETS1166_15_Ruhama_Dana/Transmission.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float time_in_seconds, time_in_days;
+time_in_seconds=419430400/960;//using the ASCII encoding to determine that 1 character is equal to 1 byte, figure out how many bytes per second it takes for the 400mb file
+cout<<"the time it takes for the complete transmission of the 400mb file is"<<time_in_seconds<<"second"<<endl;
+time_in_days=time_in_seconds/86400;//convert 24 hours into seconds 
+cout<<"the time it takes for the complete transmission of the 400mb file is"<<time_in_days<<"days"<<endl;
+  
+  return 0;
+}
