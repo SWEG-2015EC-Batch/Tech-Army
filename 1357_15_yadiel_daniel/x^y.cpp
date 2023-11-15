a74f5b0e8d5377c5c0e77460c7263149f8fa0993
Yadiel daniel <149172873+yadieldaniel@users.noreply.github.com>
2023-11-14 23:55:43 -0800

    Create x^y.cpp

---
+++ 1357_15_yadiel_daniel/x^y.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  int x,y,z;
+  cout<<"enter the first number"<<endl;
+  cin>>x;
+  cout<<"enter the second number"<<endl;
+  cin>>y;
+  z=pow(x,y);
+  cout<<"the value of the first number raised to the second number is"<<z<<endl;
+  return 0;
+  
+  
+}
