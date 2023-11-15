a52ca75e4afea6e09ce689b5d1ebd527301f513d
Yadiel daniel <149172873+yadieldaniel@users.noreply.github.com>
2023-11-14 23:59:32 -0800

    Create salary.cpp

---
+++ 1357_15_yadiel_daniel/salary.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+const float pension=0.05;
+ const float tax=0.15;
+
+int main(){
+  float workhr, brate,bsalary,gsalary,nsalary,bpay;//workhr=weekly working hours, brate=bonus rate per hour, bsalary=base salary, 
+//gsalary=gross salary, nsalary=net salary and bpay=bonus payment 
+string name; 
+  cout<<"please enter your full name:"<<endl;
+  cin>>name;
+  cout<<"please enter your weekly working hours:"<<endl;
+  cin>>workhr;
+  cout<<"please enter your base salary:"<<endl;
+  cin>>bsalary;
+  cout<<"please enter the bonus rate per hour:"<<endl;
+  cin>>brate;
+  gsalary=(workhr*brate)+bsalary;
+  cout<<"The gross salary is:"<<gsalary<<endl;
+  nsalary=gsalary-((pension*gsalary)+(tax*gsalary));
+  cout<<"the net salary is:"<<nsalary<<endl;
+  bpay=brate*workhr;
+  cout<<"the bonus payment is:"<<bpay<<endl;
+  return 0;
+  
+}
