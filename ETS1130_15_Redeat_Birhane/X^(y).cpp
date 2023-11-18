94392e2f56f05ee38a89c22ea10c0b94d73051dd
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-11-18 10:15:41 +0300

    X^(y).cpp

--- ETS1130_15_Redeat_Birhane/X^(y).cpp
+++ ETS1130_15_Redeat_Birhane/X^(y).cpp
@@ -1,17 +1,15 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int x, y;
+    double x, y;
     cout << "Enter the value of x: ";
     cin >> x;
     cout << "Enter the value of y: ";
     cin >> y;
 
-    int result = 1;
-    for (int i = 1; i <= y; ++i) {
-        result *= x;
-    }
+    double result = pow(x, y);
 
     cout << "The result of " << x << "^" << y << " is: " << result << endl;
 
