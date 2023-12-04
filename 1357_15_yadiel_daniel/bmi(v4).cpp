c8dd1feee4fe08e1f1b8533624651c2f26fc1320
Yadiel daniel <149172873+yadieldaniel@users.noreply.github.com>
2023-12-04 13:03:30 -0800

    Create bmi(v4).cpp

---
+++ 1357_15_yadiel_daniel/bmi(v4).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  double weight,height,bmi;
+  char gender;
+  int num;
+  cout<<"enter the number of people you want to calculate bmi for"<<endl;
+  cin>>num;
+  for(int x=1;x<=num;++x){
+cout<<"enter your gender as either m or f"<<endl;
+  cin>>gender;
+  cout<<"enter your weight in kilogram "<<endl;
+  cin>>weight;
+  cout<<"enter your height in meter"<<endl;
+  cin>>height;
+  bmi=weight/(pow(height,2));
+  
+   if(gender== 'f'||gender=='F'){
+     if(bmi<18.5){
+       cout<<"person #"<<x<<"you are underweight "<<endl;
+     }else if(bmi>=18.5&&bmi<24){
+       cout<<"person"<<x<<"you weight is normal"<<endl;
+       
+     }else if(bmi>=24&&bmi<30){
+       cout<<"person"<<x<<"you are overweight "<<endl;
+     }else{
+       cout<<"person"<<x<<"you are obese"<<endl;
+   }
+   } else if(gender=='m'||gender=='M'){
+     if(bmi<20.5){
+       cout<<"person"<<x<<"you are underweight "<<endl;
+       
+     }else if(bmi>=25&&bmi<30){
+       cout<<"person"<<x<<"you are overweight"<<endl;
+     }else if(bmi>30){
+       cout<<"person"<<x<<"you are obese "<<endl;
+     }else if(bmi>=20.5&&bmi<25){
+       cout<<"person"<<x<<"your weight is normal"<<endl;
+     }} else{
+       cout<<"invalid input"<<endl;}
+  } 
+return 0;
