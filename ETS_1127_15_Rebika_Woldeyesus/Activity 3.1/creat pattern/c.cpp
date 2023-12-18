3a85f8975010612944083e9c21e569a9c88e2f83
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 22:00:39 +0300

    Update c.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/c.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/c.cpp
@@ -1,1 +1,30 @@
- 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end, rows, columns;
+
+    cout << "Enter the starting number: ";
+    cin >> start;
+
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    cout << "Enter the number of columns: ";
+    cin >> columns;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            int number = start + (i * columns) + j;
+            if (number <= end) {
+                cout << number << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+} 
