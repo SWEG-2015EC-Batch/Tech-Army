466a5489e19d04ead89f72f21102f6f8af35bd22
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 23:46:47 +0300

    Full pyramid.cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Full pyramid.cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Full pyramid.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int base = 6;
+
+    for(int i = 1; i <= base; i++){
+        for(int j = base - i; j > 0; j--){
+            cout << "  ";
+        }
+        for(int k = 0; k < i; k++){
+            cout << "*   ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}7
 
