fab52253e91651c14710369a94a36bb1912f57ad
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2023-12-18 11:48:26 +0300

    Update and rename ETS1137_15_Rediet_Geremew/Activity3_1/Arithmetic_calculator to ETS1137_15_Rediet_Geremew/Activity3_1/Basic calculator application/Arithmetic_calulator.cpp

---
+++ ETS1137_15_Rediet_Geremew/Activity3_1/Basic calculator application/Arithmetic calulator.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, result;
+    char operation;
+    
+    
+    do
+    {
+        cout << "choose the operation(+,-,*,/) you want or 0 to exit";
+        cin >> operation;
+        if (operation == 0){
+            cout<<"End";
+            break;
+        }
+        cout << "Enter num1";
+        cin >> num1;
+        cout << "Enter num2";
+        cin >> num2;
+
+        switch (operation)
+        {
+        case '+':
+            result = num1 + num2;
+            cout << "your result is" << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "your result is " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "your result is" << result << endl;
+            break;
+        case '/':
+            if (num2 == 0)
+            {
+                cout << "error";
+            }
+            else
+            {
+                result = num1 / num2;
+                cout << "your result is " << result << endl;
+            }
+            break;
+        }
+        
+    } while (true);
+    return 0;
+}
