3cedcbb0285e7f9a027b5b8b7fd40cad2156c9d6
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 22:23:17 +0300

    Update symbol checker.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/symbol check/symbol checker.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/symbol check/symbol checker.cpp
@@ -5,6 +5,24 @@ int main(){
   cout<<"enter any character:";
   cin>>unknown;
   if(isalpha(unknown)){
+    switch (unknown)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case '0':
+    case 'U':
+        cout << "you entered vowel letter" << endl;
+        break;
+    default:
+        cout << "you entered consonant letter" << endl;
+        break;
+    }
     if(isupper(unknown)){
       cout<<"you entered alphabet in upper case"<<endl;
     }else if(islower(unknown)){
@@ -22,4 +40,4 @@ int main(){
   }else{
     cout<<"you entered a special character"<<endl;
   }
-}
+}//exercise number 3
