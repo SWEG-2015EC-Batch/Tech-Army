2f044ab1076561278a1781560afb2c9dcf5b5b94
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 23:16:16 +0300

    Pattern 15.cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern 15.cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern 15.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream>
+
+int main(){
+     for(int i = 0; i < 5; i++){
+        char letter = 'A';
+        for(int k = 0; k <= i; k++, letter++)
+            std::cout << letter << " ";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
