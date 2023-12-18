3f8b74044965e43334849a798f1d02728605e749
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 23:25:24 +0300

    Update hollow pyramid.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/hollow pyramid.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/hollow pyramid.cpp
@@ -1,1 +1,31 @@
+ #include <iostream>
+usng namespace std;
+int main() {
+    int n;
+
+   cout << "Enter the number of rows for the pyramid: ";
+cin >> n;
+
  
+    for (int i = 1; i <= n; ++i) {
+     
+        for (int j = i; j < n; ++j) {
+           cout << " ";
+        }
+
+  
+        for (int k = 1; k <= (2 * i - 1); ++k) {
+            
+            if (k == 1  k == (2 * i - 1)  i == n) {
+              :cout << "*";
+            } else {
+            cout << " ";
+            }
+        }
+
+        
+    cout << endl;
+    }
+
+    return 0;
+}
