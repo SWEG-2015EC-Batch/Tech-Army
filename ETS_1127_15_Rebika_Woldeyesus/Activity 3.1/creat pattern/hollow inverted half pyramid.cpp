cdcda1914e6e81f9b4aad6df9637b3dd0bed8e77
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 23:23:41 +0300

    Update hollow inverted half pyramid.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/hollow inverted half pyramid.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/hollow inverted half pyramid.cpp
@@ -1,1 +1,21 @@
- 
+ #include <iostream>
+using namespace std;
+int main() {
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            if (i == rows  j == 1  j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
