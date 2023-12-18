89dfe73e3296a9e200c987dc05d70b09a56440f9
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 23:48:17 +0300

    Inverted full pyramid.cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Inverted full pyramid.cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Inverted full pyramid.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int base = 6;
 
+    for(int i = base; i >= 1; i--){
+        for(int j = 0; j < base - i; j++){
+            cout << "  ";
+        }
+        for(int k = 0; k < i; k++){
+            cout << "*   ";
+        }
+        cout << endl;
+    }
