724e39a8b6dcc74df50cda8bb7b05e0fe05a0243
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 21:36:44 +0300

    Update symbol checker.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/symbol check/symbol checker.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/symbol check/symbol checker.cpp
@@ -1,1 +1,25 @@
- 
+#include<iostream>
+using namespace std;
+int main(){
+  char unknown;
+  cout<<"enter any character:";
+  cin>>unknown;
+  if(isalpha(unknown)){
+    if(isupper(unknown)){
+      cout<<"you entered alphabet in upper case"<<endl;
+    }else if(islower(unknown)){
+      cout<<"you entered an alpahabet in lowercase"<<endl;
+    }
+  } else if(isdigit(unknown)){
+    cout<<"you entered a digit"<<endl;
+    if(unknown%2==0){
+      cout<<"you entered an even number"<<endl;
+      
+    }else{
+      cout<<"you entered an odd number"<<endl;
+    }
+    
+  }else{
+    cout<<"you entered a special character"<<endl;
+  }
+}
