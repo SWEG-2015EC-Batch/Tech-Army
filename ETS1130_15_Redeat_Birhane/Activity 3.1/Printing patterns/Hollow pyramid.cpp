7a72771e0db17ec0e98c0004921e33645dc7565c
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 23:50:20 +0300

    Hollow pyramid.cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Hollow pyramid.cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Hollow pyramid.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int base = 6;
+
+    for(int i = 1; i <= base; i++){
+        for(int j = base - i; j > 0; j--){
+            cout << "  ";
+        }
+        for(int k = 0; k < i; k++){
+            if(k == 0 k == i - 1 i == base)
+                cout << "*   ";
+            else
+                cout << "    ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
 
