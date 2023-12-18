ec3bc467189de07479cf805396062c7c64f50347
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 21:42:07 +0300

    Pattern(full).cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern(full).cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern(full).cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int start = 10;
+    int end = 49;
+    int rows = 4;
+    int columns = 10;
 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            int number = start + (i * columns) + j;
+            if (number <= end) {
+              cout << number << " ";
+            }
+        }
+     cout << endl;
+    }
+
+    return 0;
+}
