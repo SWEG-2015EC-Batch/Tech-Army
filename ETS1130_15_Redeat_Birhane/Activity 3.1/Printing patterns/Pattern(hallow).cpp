ddaed5376c981d83d5eb2ed790fbf39745348ef2
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2023-12-18 22:39:49 +0300

    Pattern(hallow).cpp

--- ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern(hallow).cpp
+++ ETS1130_15_Redeat_Birhane/Activity 3.1/Printing patterns/Pattern(hallow).cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int rows = 7; // number of rows in the pattern
+    int cols = 8; // number of columns in the pattern
+
+    // loop through each row
+    for (int i = 1; i <= rows; i++) {
+        // loop through each column
+        for (int j = 1; j <= cols; j++) {
+            // check if we're on the first or last row or column
+            if (i == 1  i == rows  j == 1 || j == cols) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
