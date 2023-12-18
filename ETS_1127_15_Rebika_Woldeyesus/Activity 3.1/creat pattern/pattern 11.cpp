8b3689f573bea6ea536f4d2c8bb09c3a780fab6f
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 23:46:31 +0300

    Update pattern 11.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/pattern 11.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/pattern 11.cpp
@@ -1,1 +1,14 @@
- 
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i = 6; i > 0; i--){
+        for(int j = 0; j < i; j++)
+            if(i == 6 || j == 0 || j == i - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        cout << endl;
+    }
+
+    return 0;
+} 
