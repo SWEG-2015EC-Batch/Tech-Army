6edc199440b41e04af4f6f9836b5e6a09ea42622
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2023-12-18 23:36:52 +0300

    Update pattern 14.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/pattern 14.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 3.1/creat pattern/pattern 14.cpp
@@ -1,1 +1,12 @@
- 
+#include <iostream>
+using namespace std;
+int main(){
+    char letter = 'A';
+    for(int i = 0; i < 4; i++){
+        for(int k = 0; k < 6; k++, letter++)
+            cout << letter << " ";
+        cout << endl;
+    }
+
+    return 0;
+} 
