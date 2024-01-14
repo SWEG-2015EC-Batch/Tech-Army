f7bd701be6631ce933697bd796d0e28b3d4048a1
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2024-01-14 22:19:06 +0300

    Update #3.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/2D array/#3.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/2D array/#3.cpp
@@ -1,1 +1,35 @@
- 
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int testScore = 85;
+    int assignmentScore = 75;
+    int quizScore = 90;
+    int projectScore = 80;
+    int finalScore = 88;
+
+    int totalScore = testScore + assignmentScore + quizScore + projectScore + finalScore;
+
+    cout << "--------------------------------------------" << endl;
+    cout << "| Test | Assign | Quiz | Project | Final | Total |" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "|  " << testScore << "   |   " << assignmentScore << "    |   " << quizScore << "  |   " << projectScore << "     |   " << finalScore << "   |   " << totalScore << "   |" << endl;
+    cout << "--------------------------------------------" << endl;
+
+
+    cout << "Score Level: ";
+    if (totalScore >= 80) {
+        cout << "Excellent" << endl;
+    } else if (totalScore >= 60) {
+        cout << "Very Good" << endl;
+    } else if (totalScore >= 50) {
+        cout << "Fair" << endl;
+    } else if (totalScore >= 40) {
+        cout << "Poor" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
+} 
