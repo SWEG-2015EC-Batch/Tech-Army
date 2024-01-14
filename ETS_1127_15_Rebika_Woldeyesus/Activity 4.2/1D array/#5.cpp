353dcb8abdc42c83d52221a824f8a092f616d36a
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2024-01-14 20:23:13 +0300

    Update #5.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/1D array/#5.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/1D array/#5.cpp
@@ -1,1 +1,14 @@
- 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double volts[] = {11.95, 16.32, 12.15, 8.22, 15.98, 26.22, 13.54, 6.45, 17.59};
+
+
+    for (int i = 0; i < 9; i += 3) {
+        cout << setw(8) << volts[i] << setw(8) << volts[i + 1] << setw(8) << volts[i + 2] << endl;
+    }
+
+    return 0;
+} 
