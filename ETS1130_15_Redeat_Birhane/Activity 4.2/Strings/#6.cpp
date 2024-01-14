9b5da24e7e9a1b672b29b5c964afd78acfeafa99
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2024-01-14 22:22:26 +0300

    #6.cpp

--- ETS1130_15_Redeat_Birhane/Activity 4.2/Strings/#6.cpp
+++ ETS1130_15_Redeat_Birhane/Activity 4.2/Strings/#6.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
 
+void findFrequency(const string& input) {
+    int vowelCount = 0, consonantCount = 0, digitCount = 0, specialCharCount = 0;
+
+    for (char ch : input) {
+        if (isalpha(ch)) {
+            ch = tolower(ch); 
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowelCount++;
+            } else {
+                consonantCount++;
+            }
+        } else if (isdigit(ch)) {
+            digitCount++;
+        } else {
+            specialCharCount++;
+        }
+    }
+
+    cout << "Vowels: " << vowelCount << endl;
+    cout << "Consonants: " << consonantCount << endl;
+    cout << "Digits: " << digitCount << endl;
+    cout << "Special Characters: " << specialCharCount << endl;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    findFrequency(input);
+
+    return 0;
+}
