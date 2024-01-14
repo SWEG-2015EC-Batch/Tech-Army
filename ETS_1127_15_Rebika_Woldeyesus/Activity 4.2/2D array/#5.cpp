b9dd0bd9b8442055ca7557bad581985d6774ec80
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2024-01-14 20:31:27 +0300

    Update #5.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/2D array/#5.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/2D array/#5.cpp
@@ -1,1 +1,30 @@
- 
+#include <iostream>
+using namespace std;
+int main() {
+
+    bool friendships[5][5] = {
+        {false, true, false, true, true},
+        {true, false, true, false, true},
+        {false, true, false, false, false},
+        {true, false, false, false, true},
+        {true, true, false, true, false}
+    };
+
+
+    int friendship_pairs = 0;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = i + 1; j < 5; ++j) {
+            if (friendships[i][j]) {
+                friendship_pairs++;
+            }
+        }
+    }
+
+
+    std::cout << "Number of friendship pairs: " << friendship_pairs << std::endl;
+
+    return 0;
+
+
+
+} 
