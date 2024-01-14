f3f863d3ca0c802068913a4eb52f50079a7564b0
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2024-01-14 23:55:40 +0300

    Update #8.cpp

--- ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/string/#8.cpp
+++ ETS_1127_15_Rebika_Woldeyesus/Activity 4.2/string/#8.cpp
@@ -1,1 +1,21 @@
- 
+#include<iostream>
+#include<algorithm>
+#include<cctype>
+using namespace std;
+
+int main() {
+    string inputText;
+
+    cout << "Enter a line of text: ";
+    getline(cin, inputText);
+
+
+    transform(inputText.begin(), inputText.end(), inputText.begin(), ::toupper);
+    cout << "Uppercase: " << inputText << endl;
+
+
+    transform(inputText.begin(), inputText.end(), inputText.begin(), ::tolower);
+    cout << "Lowercase: " << inputText << endl;
+
+    return 0;
+} 
