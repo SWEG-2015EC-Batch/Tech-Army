b048e211696f2ad5a89c6c06ee6df3d1a99f889a
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2024-01-14 22:40:13 +0300

    Create qn#6.cpp

---
+++ ETS1137_15_Rediet_Geremew/Activity 4.2/1D Array/qn#6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int totalMark;
+    string scoreLevel;
+    cout << "Enter the total mark: ";
+    cin >> totalMark;
+
+    if (totalMark >= 80) {
+        scoreLevel = "Excellent";
+    } else if (totalMark >= 60) {
+        scoreLevel = "Very Good";
+    } else if (totalMark >= 50) {
+        scoreLevel = "Fair";
+    } else if (totalMark >= 40) {
+        scoreLevel = "Poor";
+    } else {
+        scoreLevel = "Fail";
+    }
+
+    cout << "---------------------------" << endl;
+    cout << "| Total Mark | Score Level |" << endl;
+    cout << "---------------------------" << endl;
+    cout << "| " << setw(10) << totalMark << " | " << setw(11) << scoreLevel << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    return 0;
+}
