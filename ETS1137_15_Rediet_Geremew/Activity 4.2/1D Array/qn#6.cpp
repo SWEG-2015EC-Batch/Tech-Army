83e5055e37d3fac79e99aadd61c843758a2444c3
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2024-01-23 15:11:49 +0300

    Update qn#6.cpp

--- ETS1137_15_Rediet_Geremew/Activity 4.2/1D Array/qn#6.cpp
+++ ETS1137_15_Rediet_Geremew/Activity 4.2/1D Array/qn#6.cpp
@@ -3,28 +3,48 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
-    int totalMark;
-    string scoreLevel;
-    cout << "Enter the total mark: ";
-    cin >> totalMark;
+int main()
+{
+    int numStudent;
 
-    if (totalMark >= 80) {
-        scoreLevel = "Excellent";
-    } else if (totalMark >= 60) {
-        scoreLevel = "Very Good";
-    } else if (totalMark >= 50) {
-        scoreLevel = "Fair";
-    } else if (totalMark >= 40) {
-        scoreLevel = "Poor";
-    } else {
-        scoreLevel = "Fail";
+    cout << "Enter the number of students ";
+    cin >> numStudent;
+    float totalMark[numStudent];
+    string scoreLevel[numStudent];
+    for (int i = 0; i < numStudent; i++)
+    {
+        cout << "Enter the total mark of student " << i + 1 << ": ";
+        cin >> totalMark[i];
+        if (totalMark[i] >= 80)
+        {
+            scoreLevel[i] = "Excellent";
+        }
+        else if (totalMark[i] >= 60)
+        {
+            scoreLevel[i] = "Very Good";
+        }
+        else if (totalMark[i] >= 50)
+        {
+            scoreLevel[i] = "Fair";
+        }
+        else if (totalMark[i] >= 40)
+        {
+            scoreLevel[i] = "Poor";
+        }
+        else
+        {
+            scoreLevel[i] = "Fail";
+        }
     }
 
     cout << "---------------------------" << endl;
     cout << "| Total Mark | Score Level |" << endl;
-    cout << "---------------------------" << endl;
-    cout << "| " << setw(10) << totalMark << " | " << setw(11) << scoreLevel << " |" << endl;
+    for (int i = 0; i < numStudent; i++)
+    {
+        cout << "---------------------------" << endl;
+        cout << "| " << setw(10) << totalMark[i] << " | " << setw(11) << scoreLevel[i] << " |" << endl;
+    }
+
     cout << "---------------------------" << endl;
 
     return 0;
