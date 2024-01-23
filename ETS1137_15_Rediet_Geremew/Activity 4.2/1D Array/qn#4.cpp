125caf8f6bb8c32960a98ed90feed3c7595f4c1f
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2024-01-23 14:45:10 +0300

    Update qn#4.cpp

--- ETS1137_15_Rediet_Geremew/Activity 4.2/1D Array/qn#4.cpp
+++ ETS1137_15_Rediet_Geremew/Activity 4.2/1D Array/qn#4.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   
-    int first[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int second[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
-    int result[3][3];
+int main()
+{
 
-    
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    int first[3][3];
+    int second[3][3];
+    int result[3][3];
+    cout << "First list" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "enter number " << i + 1 << "." << j + 1;
+            cin >> first[i][j];
+        }
+    }
+    cout << "Second list" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "enter number " << i + 1 << "." << j + 1;
+            cin >> second[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             result[i][j] = first[i][j] + second[i][j];
         }
     }
 
-    
     cout << "Result:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cout << result[i][j] << "\t";
         }
         cout << endl;
