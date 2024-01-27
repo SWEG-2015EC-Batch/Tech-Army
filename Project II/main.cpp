44c707995f9c3d172d9cbca19afe11d3af0552cb
Rebika Woldeyesus <149299108+reb-ika@users.noreply.github.com>
2024-01-27 17:32:00 +0300

    Update main.cpp

--- Project II/main.cpp
+++ Project II/main.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Print upper part
+    for (int i = 0; i <= 9; ++i) {
+        // Print spaces
+        for (int j = 0; j < i; ++j) {
+            cout << "  ";
+        }
 
+        // Print increasing numbers upto 9
+        for (int k = 0; k <= 9 - i; ++k) {
+            cout << k << " ";
+        }
+
+        // Print decreasing numbers upto 0
+        for (int k = 9 - i - 1; k >= 0; --k) {
+            if (k > 0) {
+                cout << k << " ";
+            } else {
+                cout << k;
+            }
+        }
+
+        cout << endl;
+    }
+
+    // Print lower part
+    for (int i = 1; i <= 9; ++i) {
+        // Print spaces
+        for (int j = 0; j < 9 - i; ++j) {
+            cout << "  ";
+        }
+
+        // Print increasing numbers upto 9
+        for (int k = 0; k <= i; ++k) {
+            cout << k << " ";
+        }
+
+        // Print decreasing numbers upto 0
+        for (int k = i - 1; k >= 0; --k) {
+            if (k > 0) {
+                cout << k << " ";
+            } else {
+                cout << k;
+            }
+        }
+
+        cout << endl;
+    }
+
+
+    return 0;
+}
